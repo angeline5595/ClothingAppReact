596edcca67546cea39cc6144b6cf0b10adfde21f
angeline5595 <sylviaangeline5@gmail.com>
2021-03-17 16:15:30 +0100

    added local storage  redux persisit to the app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,20 @@
 import {combineReducers} from 'redux';
-
 import userReducer from './user-reducer-action/user.reducer';
 import cartReducer from './cart-reducer/cart.reducer';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// this is for session storsge:import sessionStorage from 'redux-presist/lib/storage';
 
-export default combineReducers(
-  {
-    user:userReducer,
-    cart:cartReducer
-  }
-);
+export const persistConfig={
+  key:'root',
+  storage,
+  whitelist:['cart']
+};
+
+
+export const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
